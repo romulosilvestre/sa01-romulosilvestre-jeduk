7539d417e249a4f6688bab0a17806f6cea7b0903
romulo.silvestre <romulo.silvestre@df.estudante.senai.br>
2023-01-31 18:03:18 -0300

    projeto educacional - iniciando professor

---
+++ src/sa01jeduk/Main.java
@@ -0,0 +1,21 @@
+package sa01jeduk;
+
+import javax.swing.JOptionPane;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = JOptionPane.showInputDialog("Digite o nome:");
+    String saString = JOptionPane.showInputDialog("Digite o sa:");
+    int sa = Integer.parseInt(saString);
+    String status = JOptionPane.showInputDialog("Digite o status:");
+    
+    try (FileWriter writer = new FileWriter("fontededados.txt", true)) {
+      writer.write(nome + "," + sa + "," + status + "\n");
+      JOptionPane.showMessageDialog(null, "Dados salvos com sucesso!");
+    } catch (IOException e) {
+      JOptionPane.showMessageDialog(null, "Erro ao salvar dados: " + e.getMessage());
+    }
+  }
+}
